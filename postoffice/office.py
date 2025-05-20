a7ab46a05d5d594baa1f8c660440ec27e7e49afc
ScorpioGK <scorpiogameking@gmail.com>
2025-05-20 18:33:39 -0400

    Update office.py

--- postoffice/office.py
+++ postoffice/office.py
@@ -30,11 +30,10 @@ class Office():
     def setActiveMailman(self, username:str):
         self.logger.info(f"Searching for Mailman [{self._mailmen[username]}]")
         self._activeMailman = self._mailmen[username]
-        self.logger.info(f"Setting Service Username [{self._activeMailman.name}]")
-        self._office_service.username = self._activeMailman.name
         self.logger.info(f"Setting Service Password [{self._activeMailman._password}]")
         self._office_service.password = self._activeMailman._password
-        self.logger.info(f"Setting Mailman [{self._mailmen[username]}] to Active")
+        self.logger.info(f"Setting Service Username [{self._activeMailman.name}]")
+        self._office_service.username = self._activeMailman.name
     
     def sendMail(self, topic:str, send_to:list[str], body:str):
         self._activeMailman.sendMail(self._office_service, topic, send_to, body)
